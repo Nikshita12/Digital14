bac826f7d1a9cb61c60ca8bec48cb2cdaba1ec7e
Nikshita12 <69782627+Nikshita12@users.noreply.github.com>
2020-08-21 20:57:44 +0530

    Second Commit

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use( express.json({ extended : false}));
 app.get('/', (req, res) => res.send('API running'));
 
 // Define routes
+
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
