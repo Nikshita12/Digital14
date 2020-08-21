6ee3d4151b7ad8904c7744a1013546214b8ccc91
Nikshita12 <69782627+Nikshita12@users.noreply.github.com>
2020-08-21 10:09:52 +0530

    Second commit

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
+
 process.on('SIGINT', function() {
     console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
     // some other closing procedures go here
